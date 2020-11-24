cacbb0b3965b7dd0b886f63783686b10983393ea
smnlpz <simondelosbros@correo.ugr.es>
2020-11-24 17:34:15 +0100

    Nuevos ejemplos

--- generate_examples.sh
+++ generate_examples.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 
-python3 run.py images/troyita.png images/mosaic/ 100 3000 results/average/troyita_average_100.jpg
-python3 run.py images/troyita.png images/mosaic/ 150 3000 results/average/troyita_average_150.jpg
-python3 run.py images/troyita.png images/mosaic/ 300 3000 results/average/troyita_average_300.jpg
+python3 run.py images/artificial_man.jpg         images/mosaic_24 0 100 2000    results/mosaic_24_rep_0/artificial_man_100.jpg
+python3 run.py images/artificial_woman.jpg       images/mosaic_24 0 100 2000    results/mosaic_24_rep_0/artificial_woman_100.jpg
+python3 run.py images/flores.png                 images/mosaic_24 0 100 2000    results/mosaic_24_rep_0/flores_100.jpg
+python3 run.py images/troyita_buenasnoches.png   images/mosaic_24 0 100 2000    results/mosaic_24_rep_0/troyita_buenasnoches_100.jpg
 
-python3 run.py images/troyita_buenasnoches.png images/mosaic/ 100 3000 results/average/troyita_buenasnoches_average_100.jpg
-python3 run.py images/troyita_buenasnoches.png images/mosaic/ 150 3000 results/average/troyita_buenasnoches_average_150.jpg
-python3 run.py images/troyita_buenasnoches.png images/mosaic/ 300 3000 results/average/troyita_buenasnoches_average_300.jpg
+python3 run.py images/artificial_man.jpg         images/mosaic_64 0 100 2000    results/mosaic_64_rep_0/artificial_people_100.jpg
+python3 run.py images/artificial_woman.jpg       images/mosaic_64 0 100 2000    results/mosaic_64_rep_0/artificial_woman_100.jpg
+python3 run.py images/flores.png                 images/mosaic_64 0 100 2000    results/mosaic_64_rep_0/flores_100.jpg
+python3 run.py images/troyita_buenasnoches.png   images/mosaic_64 0 100 2000    results/mosaic_64_rep_0/troyita_buenasnoches_100.jpg
 
-python3 run.py images/flores.png images/mosaic/ 100 3000 results/average/flores_average_100.jpg
-python3 run.py images/flores.png images/mosaic/ 150 3000 results/average/flores_average_150.jpg
-python3 run.py images/flores.png images/mosaic/ 300 3000 results/average/flores_average_300.jpg
+python3 run.py images/artificial_man.jpg         images/mosaic_64 1 100 2000    results/mosaic_64_rep_1/artificial_people_100.jpg
+python3 run.py images/artificial_woman.jpg       images/mosaic_64 1 100 2000    results/mosaic_64_rep_1/artificial_woman_100.jpg
+python3 run.py images/flores.png                 images/mosaic_64 1 100 2000    results/mosaic_64_rep_1/flores_100.jpg
+python3 run.py images/troyita_buenasnoches.png   images/mosaic_64 1 100 2000    results/mosaic_64_rep_1/troyita_buenasnoches_100.jpg
+
+python3 run.py images/artificial_man.jpg         images/mosaic_64 2 100 2000    results/mosaic_64_rep_2/artificial_people_100.jpg
+python3 run.py images/artificial_woman.jpg       images/mosaic_64 2 100 2000    results/mosaic_64_rep_2/artificial_woman_100.jpg
+python3 run.py images/flores.png                 images/mosaic_64 2 100 2000    results/mosaic_64_rep_2/flores_100.jpg
+python3 run.py images/troyita_buenasnoches.png   images/mosaic_64 2 100 2000    results/mosaic_64_rep_2/troyita_buenasnoches_100.jpg
+
+python3 run.py images/artificial_man.jpg         images/mosaic_64 3 100 2000    results/mosaic_64_rep_3/artificial_people_100.jpg
+python3 run.py images/artificial_woman.jpg       images/mosaic_64 3 100 2000    results/mosaic_64_rep_3/artificial_woman_100.jpg
+python3 run.py images/flores.png                 images/mosaic_64 3 100 2000    results/mosaic_64_rep_3/flores_100.jpg
+python3 run.py images/troyita_buenasnoches.png   images/mosaic_64 3 100 2000    results/mosaic_64_rep_3/troyita_buenasnoches_100.jpg
+
